8a1248cb187973ed7d4ff065955dce0c724788ca
macbook <quanghoa.trinh@gmail.com>
2022-12-29 20:03:05 +0700

    api - thread

--- T2203E_CSharp/Program.cs
+++ T2203E_CSharp/Program.cs
@@ -2,11 +2,85 @@
 using T2203E_CSharp.session3;
 using T2203E_CSharp.contact;
 using T2203E_CSharp.session4;
+using T2203E_CSharp.session5;
 
 public class Program
 {
     static void Main(string[] args)
     {
+        Fetch();
+    }
+
+    static async void Fetch()
+    {
+        CallApi ca = new CallApi();
+        Product s = await ca.FetchData();
+        Console.WriteLine(s.ToString());
+    }
+
+    static void Main6(string[] args)
+    {
+        //ThreadRun();
+        //Thread t1 = new Thread(ThreadRun);
+        //Thread t2 = new Thread(ThreadRunParam);
+        //t2.IsBackground = true;
+        ////t1.IsBackground = true;
+        //t1.Start();
+        //t2.Start("T2203E");
+
+        Number num = new Number() { X = 0, Y = 0 };
+        Thread n1 = new Thread(ThreadLock);
+        Thread n2 = new Thread(ThreadLock);
+        n1.Start(num);
+        n2.Start(num);
+    }
+
+    static void ThreadLock(object o)
+    {
+        Number n = (Number)o;
+        for(int i = 0; i < 20; i++)
+        {
+            lock (n)
+            {
+                n.ChangeXY();
+                n.PrintXY();
+            }
+            try
+            {
+                Thread.Sleep(1000);
+            }
+            catch (Exception e) { }
+        }
+    }
+
+    static void ThreadRun()
+    {
+        for(int i = 0; i < 20; i++)
+        {
+            Console.WriteLine("i=" + i);
+            try
+            {
+                Thread.Sleep(1000);
+            }catch(Exception e) { }
+        }
+    }
+
+    static void ThreadRunParam(object msg)
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            Console.WriteLine("i=" + i+" Msg:"+msg);
+            try
+            {
+                Thread.Sleep(1000);
+            }
+            catch (Exception e) { }
+        }
+    }
+
+    static void Main5(string[] args)
+    {
+      
         StringToVoid stv = new StringToVoid(ShowMessage);
         StringToVoid stv2 = new StringToVoid(DemoDelegate.SayHello);
         StringToVoid stv3 = new StringToVoid(new DemoDelegate().ShowInfo);
@@ -20,6 +94,8 @@ public class Program
 
         DemoEvent de = new DemoEvent();
         de.Invoke();
+
+
     }
 
     static void ShowMessage(string msg)
